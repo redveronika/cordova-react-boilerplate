f366591191cde3a476e537df48b8a48a8c1c37b3
Dmitry Rodnikov <dim.reg@mail.ru>
2017-08-16 21:57:45 +0300

    Добавил переменные entry и output

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const {
-  HotModuleReplacementPlugin
-} = require('webpack');
+const { HotModuleReplacementPlugin } = require('webpack');
+const ENTRY_POINT = '../src/index.js';
+const OUTPUT_PATH = '../www';
 
 module.exports = merge([{
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    path.join(__dirname, '../src/index.js')
+    path.join(__dirname, ENTRY_POINT)
   ],
   output: {
-    path: path.join(__dirname, '../www'),
+    path: path.join(__dirname, OUTPUT_PATH),
     filename: 'bundle.js',
     publicPath: 'http://localhost:8080/'
   },
@@ -24,13 +24,11 @@ module.exports = merge([{
       loader: 'babel-loader'
     }]
   },
-  plugins: [
-    new HotModuleReplacementPlugin(),
-  ],
+  plugins: [ new HotModuleReplacementPlugin() ],
   devServer: {
     stats: 'errors-only',
     host: 'localhost',
     port: 8080,
-    contentBase: path.join(__dirname, '../www')
+    contentBase: path.join(__dirname, OUTPUT_PATH)
   }
 }]);
